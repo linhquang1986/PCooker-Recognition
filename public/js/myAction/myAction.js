be4b2d1d303409c6ba5c8943ab5e7ab8182eff0b
vupn199955@gmail.com <vupn199955@gmail.com>
2017-11-27 17:58:26 +0700

    update

--- public/js/myAction/myAction.js
+++ public/js/myAction/myAction.js
@@ -11,6 +11,7 @@ function _startorder() {
         renderMenu(data)
     })
 }
+_startorder();
 
 function _call() {
     responsiveVoice.speak("Bạn muốn tôi giúp gì", "Vietnamese Male")
@@ -33,46 +34,52 @@ function updateBill(_drink) {
         billData.filter(drink => drink.toLowerCase() !== _drink.toLowerCase())
     }
 }
-function checkAbortDrink(msg) {
-    let abort = 0;
-    msg.search('vo') >= 0 && abort++;
-    msg.search('bo') >= 0 && abort++;
-    msg.search('huy') >= 0 && abort++;
-    msg.search('vo') >= 0 && abort++;
-    msg.search('xoa') >= 0 && abort++;
-    return abort;
-}
-function locdau(_str) {
-    var str;
-    str = _str;
-    str = str.toLowerCase();
-    str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, "a");
-    str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, "e");
-    str = str.replace(/ì|í|ị|ỉ|ĩ/g, "i");
-    str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, "o");
-    str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, "u");
-    str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, "y");
-    str = str.replace(/đ/g, "d");
-    str = str.replace(/^\-+|\-+$/g, "");
-    return str;
-}
-function addBill(_drink) {
+// function checkAbortDrink(msg) {
+//     let abort = 0;
+//     msg.search('vo') >= 0 && abort++;
+//     msg.search('bo') >= 0 && abort++;
+//     msg.search('huy') >= 0 && abort++;
+//     msg.search('vo') >= 0 && abort++;
+//     msg.search('xoa') >= 0 && abort++;
+//     return abort;
+// }
+// function locdau(_str) {
+//     var str;
+//     str = _str;
+//     str = str.toLowerCase();
+//     str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, "a");
+//     str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, "e");
+//     str = str.replace(/ì|í|ị|ỉ|ĩ/g, "i");
+//     str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, "o");
+//     str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, "u");
+//     str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, "y");
+//     str = str.replace(/đ/g, "d");
+//     str = str.replace(/^\-+|\-+$/g, "");
+//     return str;
+// }
+function addBill(_drink, _quanlity, option) {
+    drinkOption = null;
+    let quanlity = _quanlity || 1;
+    let _option = '';
+    if (option)
+        _option = '(' + option + ')';
     drinksData.forEach(drink => {
         if (_drink.toLowerCase() == drink.name.toLowerCase()) {
             horverDrink(drink._id)
             billData.push(_drink)
             let drinkInBill = $('.ordersDrinks').find(`[drinkId=${drink._id}]`);
             if (drinkInBill.length == 0) {
-                let bill = $('.orders').find('.ordersDrinks')
-                let drinkIntent = '<li class="list-group-item drink-item" drinkId="' + drink._id + '"><p class="quanlity">' + 1 + '</p>' + drink.name + '<span class="badge">' + drink.price + ' Đ</span></li>';
+                let bill = $('.orders').find('.ordersDrinks');
+                let price = quanlity * drink.price;
+                let drinkIntent = '<li class="list-group-item drink-item" drinkId="' + drink._id + '"><p class="quanlity">' + quanlity + '</p>' + drink.name + _option + '<span class="badge">' + price + ' Đ</span></li>';
                 bill.append(drinkIntent)
             } else {
                 let quanlityE = drinkInBill.find('.quanlity');
                 let badge = drinkInBill.find('.badge')
-                let quanlity = parseInt(quanlityE.html()) + 1;
-                let price = quanlity * drink.price;
+                let quanlity_ = parseInt(quanlityE.html()) + parseInt(quanlity);
+                let price = quanlity_ * drink.price;
 
-                quanlityE.html(quanlity)
+                quanlityE.html(quanlity_)
                 badge.html(price + ' Đ')
             }
         }
--- public/js/witAi.js
+++ public/js/witAi.js
@@ -1,14 +1,18 @@
 "use strict"
 var recognition = null;
 var inAction = null;
+var drinkOption = null;
 function speak(msg) {
     responsiveVoice.speak(msg, "Vietnamese Male");
 }
 function sendWitAi(msg) {
-    let abort = checkAbortDrink(locdau(msg));
+    let abort = null;
     let data = { message: msg }
     postWit('/wit/message', data, (res) => {
         console.log(res)
+        if (res.entities.abort) {
+            abort = true;
+        }
         if (res.entities.statements) {
             let action = res.entities.statements[0].value
             inAction = action;
@@ -21,11 +25,44 @@ function sendWitAi(msg) {
         if (res.entities.menus) {
             let _menu = res.entities.menus[0].value;
         }
+        if (res.entities.nhietdo) {
+            let nhietdo = res.entities.nhietdo[0].value;
+            if (drinkOption) {
+                addBill(drinkOption.name, drinkOption.quanlity, nhietdo);
+            }
+        }
         if (res.entities.drinks) {
             let _drink = res.entities.drinks[0].value;
-            if (abort == 0) {
-                addBill(_drink)
-                responsiveVoice.speak('Bạn chọn ' + _drink, "Vietnamese Male")
+            let _meta = res.entities.drinks[0].metadata;
+            let nhietdo = null;
+            let _quanlity = null;
+            if (res.entities.nhietdo) {
+                nhietdo = res.entities.nhietdo[0].value;
+            }
+            if (res.entities.quanlity) {
+                _quanlity = res.entities.quanlity[0].value;
+            }
+            if (!abort) {
+                if (_meta == 'nhietdo') {
+                    if (nhietdo) {
+                        addBill(_drink, _quanlity, nhietdo);
+                    } else {
+                        let data = {
+                            name: _drink,
+                            quanlity: _quanlity
+                        }
+                        drinkOption = data;
+                        responsiveVoice.speak('Bạn muốn nóng hay đá', "Vietnamese Male")
+                    }
+                } else {
+                    if (res.entities.quanlity) {
+                        _quanlity = res.entities.quanlity[0].value;
+                        addBill(_drink, _quanlity)
+                    }
+                    else {
+                        addBill(_drink)
+                    }
+                }
             } else {
                 updateBill(_drink)
             }
